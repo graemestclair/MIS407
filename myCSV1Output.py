24e100c65171057ef7ba2636b82d0cdc6a763af9
graemestclair <gstclair@iastate.edu>
2015-09-16 11:40:19 -0500

    HW1
    updated files to fit project assignment

---
+++ myCSV1Output.py
@@ -0,0 +1,20 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 23 2015, 02:52:03) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+================================ RESTART ================================
+>>> 
+['1/2/2014', '15', '8', 'red', '11.99']
+1/2/2014
+1/2/2014 15 8
+['1/3/2014', '5', '2', 'green', '10.99']
+1/3/2014
+1/3/2014 5 2
+['1/4/2014', '9', '1', 'blue', '10.99']
+1/4/2014
+1/4/2014 9 1
+['1/5/2014', '15', '10', 'yellow', '11.99']
+1/5/2014
+1/5/2014 15 10
+>>> 
